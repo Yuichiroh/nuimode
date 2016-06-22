8ef36fe05e495e0ea0df95fb1fdff32dd810c98b
Yuichiroh <y-matsu@ecei.tohoku.ac.jp>
2016-06-22 21:38:23 +0900

    indent of applescripts

--- src/main/scala/yuima/nuimo/action/ChromeYoutubeAction.scala
+++ src/main/scala/yuima/nuimo/action/ChromeYoutubeAction.scala
@@ -10,21 +10,20 @@ object ChromeYoutubeAction {
     AppleScript(
       """set foundYoutube to false
         |tell application "Google Chrome"
-        |	repeat with t in tabs of windows
-        |		tell t
-        |			if URL starts with "http://www.youtube.com/watch" or URL starts with "https://www.youtube.com/watch" then
-        |				execute javascript "
-        |        var player = document.getElementById('movie_player') || document.getElementsByTagName('embed')[0];
-        |
-        |        if (player) {
-        |          document.getElementsByClassName('ytp-play-button')[0].click()
-        |		    }
-        |       "
-        |				set foundYoutube to true
-        |				exit repeat
-        |			end if
-        |		end tell
-        |	end repeat
+        |  repeat with t in tabs of windows
+        |    tell t
+        |      if URL starts with "http://www.youtube.com/watch" or URL starts with "https://www.youtube.com/watch" then
+        |        execute javascript "
+        |          var player = document.getElementById('movie_player') || document.getElementsByTagName('embed')[0];
+        |          if (player) {
+        |            document.getElementsByClassName('ytp-play-button')[0].click()
+        |		       }
+        |        "
+        |        set foundYoutube to true
+        |        exit repeat
+        |      end if
+        |    end tell
+        |  end repeat
         |end tell
         |return foundYoutube
       """.stripMargin).runScriptSync.toBoolean
@@ -38,11 +37,10 @@ object ChromeYoutubeAction {
         |    tell t
         |      if URL starts with "http://www.youtube.com/watch" or URL starts with "https://www.youtube.com/watch" then
         |        execute javascript "
-        |        var player = document.getElementById('movie_player') || document.getElementsByTagName('embed')[0];
-        |
-        |        if (player) {
-        |          document.getElementsByClassName('ytp-next-button')[0].click()
-        |		    }
+        |          var player = document.getElementById('movie_player') || document.getElementsByTagName('embed')[0];
+        |          if (player) {
+        |            document.getElementsByClassName('ytp-next-button')[0].click()
+        |          }
         |        "
         |        set foundYoutube to true
         |        exit repeat
@@ -62,11 +60,10 @@ object ChromeYoutubeAction {
         |    tell t
         |      if URL starts with "http://www.youtube.com/watch" or URL starts with "https://www.youtube.com/watch" then
         |        execute javascript "
-        |        var player = document.getElementById('movie_player') || document.getElementsByTagName('embed')[0];
-        |
-        |        if (player) {
-        |          document.getElementsByClassName('ytp-prev-button')[0].click()
-        |		    }
+        |          var player = document.getElementById('movie_player') || document.getElementsByTagName('embed')[0];
+        |          if (player) {
+        |            document.getElementsByClassName('ytp-prev-button')[0].click()
+        |          }
         |        "
         |        set foundYoutube to true
         |        exit repeat
